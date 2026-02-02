8d2cef8a516147699a7d396d72d67ed850e9b498
mochineko <mochinekoo0113@gmail.com>
2026-02-02 21:48:42 +0900

    [add] 天気情報の表示を追加しました

--- Command/WeatherCommand.cs
+++ Command/WeatherCommand.cs
@@ -17,9 +17,14 @@ namespace C_Shape_DiscordBot.Command {
                     .Value
                     .ToString();
                 WeatherAPI weatherAPI = WeatherAPI.GetWeather(cityCode);
+                WeatherAPI.Forecast forecast = weatherAPI.forecast[0];
+                WeatherAPI.Forecast.Detail detail = forecast.detail;
+
                 var embed = new EmbedBuilder();
-                embed.Title = "お天気情報";
-                embed.AddField("情報", weatherAPI.forecast[0].detail.wind);
+                embed.Title = cityCode + "のお天気情報";
+                embed.AddField("天気", forecast.telop);
+                embed.AddField("風", detail.wind);
+                embed.AddField("波", detail.wave);
                 var channel = command.Channel;
                 await channel.SendMessageAsync(embed: embed.Build());
             }
